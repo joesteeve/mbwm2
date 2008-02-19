a462a0fb9010c49470c6b4626664eb9e51947b8c
Tomas Frydrych <tf@linux.intel.com>
2008-02-19 09:32:57 +0000

    2008-02-19  Tomas Frydrych  <tf@o-hand.com>
    	* configure.ac, *.am, *.h:
    	Changes for building mbwm2 as a library (based on patch by Johan
    	Bilien):
    
    	Moved 'src' to 'matchbox'; fixed up headers to use #include <>.
    	Moved config.h to matchbox/mb-wm-config.h.
    	Added --enable-libmatchbox configure option, libmatchbox2.pc.in

---
+++ matchbox/core/mb-wm.h
@@ -0,0 +1,61 @@
+/*
+ *  Matchbox Window Manager II - A lightweight window manager not for the
+ *                               desktop.
+ *
+ *  Authored By Matthew Allum <mallum@o-hand.com>
+ *
+ *  Copyright (c) 2005 OpenedHand Ltd - http://o-hand.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
+#ifndef _HAVE_MB_WM_H
+#define _HAVE_MB_WM_H
+
+#define _GNU_SOURCE 		/* For vasprintf */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>          /* for XA_ATOM etc */
+#include <X11/keysym.h>         /* key mask defines */
+
+#include <matchbox/core/xas.h>    		/* async stuff not needed for xlib on xcb */
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#include <mb-wm-macros.h>
+#include <mb-wm-debug.h>
+#include <mb-wm-types.h>
+#include <mb-wm-util.h>
+#include <mb-wm-object.h>
+#include <mb-wm-atoms.h>
+#include <mb-wm-props.h>
+#include <mb-wm-keys.h>
+#include <mb-wm-decor.h>
+#include <mb-wm-client-window.h>
+#include <mb-wm-root-window.h>
+#include <mb-wm-client.h>
+#include <mb-wm-client-base.h>
+#include <mb-wm-layout.h>
+#include <mb-wm-stack.h>
+#include <mb-window-manager.h>
+#include <mb-wm-main-context.h>
+#endif
---
+++ matchbox/theme-engines/mb-wm-theme-xml.h
@@ -0,0 +1,103 @@
+#ifndef _HAVE_MB_WM_THEME_PRIVATE_H
+#define _HAVE_MB_WM_THEME_PRIVATE_H
+
+#include <matchbox/core/mb-wm.h>
+
+/*
+ * Helper structs for xml theme
+ */
+typedef struct Button
+{
+  MBWMDecorButtonType type;
+  MBWMDecorButtonPack packing;
+
+  MBWMColor clr_fg;
+  MBWMColor clr_bg;
+
+  int x;
+  int y;
+  int width;
+  int height;
+
+  /* Needed by png themes */
+  int active_x;
+  int active_y;
+
+  int inactive_x;
+  int inactive_y;
+
+  int press_activated;
+} MBWMXmlButton;
+
+typedef struct Decor
+{
+  MBWMDecorType type;
+
+  MBWMColor clr_fg;
+  MBWMColor clr_bg;
+  MBWMColor clr_bg2;
+  MBWMColor clr_frame;
+
+  int x;
+  int y;
+  int width;
+  int height;
+  int font_size;
+  int show_title;
+
+  char * font_family;
+
+  MBWMList * buttons;
+}MBWMXmlDecor;
+
+typedef struct Client
+{
+  MBWMClientType  type;
+
+  int x;
+  int y;
+  int width;
+  int height;
+
+  Bool shaped;
+
+  MBWMList       *decors;
+
+  MBWMClientLayoutHints layout_hints;
+
+#ifdef ENABLE_COMPOSITE
+  MBWMList       *effects;
+#endif
+}MBWMXmlClient;
+
+MBWMXmlButton *
+mb_wm_xml_button_new ();
+
+void
+mb_wm_xml_button_free (MBWMXmlButton * b);
+
+MBWMXmlDecor *
+mb_wm_xml_decor_new ();
+
+void
+mb_wm_xml_decor_free (MBWMXmlDecor * d);
+
+MBWMXmlClient *
+mb_wm_xml_client_new ();
+
+void
+mb_wm_xml_client_free (MBWMXmlClient * c);
+
+MBWMXmlClient *
+mb_wm_xml_client_find_by_type (MBWMList *l, MBWMClientType type);
+
+MBWMXmlDecor *
+mb_wm_xml_decor_find_by_type (MBWMList *l, MBWMDecorType type);
+
+MBWMXmlButton *
+mb_wm_xml_button_find_by_type (MBWMList *l, MBWMDecorButtonType type);
+
+void
+mb_wm_xml_clr_from_string (MBWMColor * clr, const char *s);
+
+#endif
