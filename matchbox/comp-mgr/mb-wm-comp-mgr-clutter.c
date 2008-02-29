68034aab4dc085164a280dd41fb22a9932a29916
Tomas Frydrych <tf@linux.intel.com>
2008-02-29 18:07:06 +0000

    2008-02-29  Tomas Frydrych  <tf@o-hand.com>
    	* matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c:
    	Initial stab at gaussian shadows.

--- matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c
+++ matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c
@@ -35,6 +35,20 @@
 #include <X11/extensions/Xcomposite.h>
 #include <X11/extensions/shape.h>
 
+#include <math.h>
+
+#define SHADOW_RADIUS 6
+#define SHADOW_OPACITY	0.9
+#define SHADOW_OFFSET_X	(-SHADOW_RADIUS)
+#define SHADOW_OFFSET_Y	(-SHADOW_RADIUS)
+
+#define MAX_TILE_SZ 128 	/* make sure size/2 < MAX_TILE_SZ */
+#define WIDTH  (3*MAX_TILE_SZ)
+#define HEIGHT (3*MAX_TILE_SZ)
+
+static unsigned char *
+mb_wm_comp_mgr_clutter_shadow_gaussian_make_tile ();
+
 static void
 mb_wm_comp_mgr_clutter_add_actor (MBWMCompMgrClutter *, ClutterActor *);
 
@@ -608,6 +622,8 @@ struct MBWMCompMgrClutterPrivate
 {
   ClutterActor * stage;
   ClutterActor * desktop;
+  ClutterActor * shadow;
+
   Window         overlay_window;
   int            damage_event;
 };
@@ -616,6 +632,10 @@ static void
 mb_wm_comp_mgr_clutter_private_free (MBWMCompMgrClutter *mgr)
 {
   MBWMCompMgrClutterPrivate * priv = mgr->priv;
+
+  if (priv->shadow)
+    g_object_unref (priv->shadow);
+
   free (priv);
 }
 
@@ -1040,6 +1060,7 @@ mb_wm_comp_mgr_clutter_map_notify_real (MBWMCompMgr *mgr,
   const MBWMList            * l;
   unsigned int                shadow_clr[4];
   ClutterColor                shadow_cclr;
+  MBWMCompMgrShadowType       shadow_type;
 
   /*
    * We get called for windows as well as their children, so once we are
@@ -1062,24 +1083,65 @@ mb_wm_comp_mgr_clutter_map_notify_real (MBWMCompMgr *mgr,
   texture = clutter_x11_texture_pixmap_new ();
   clutter_actor_show (texture);
 
-  mb_wm_theme_get_shadow_color (wm->theme,
-				&shadow_clr[0],
-				&shadow_clr[1],
-				&shadow_clr[2],
-				&shadow_clr[3]);
+  shadow_type = mb_wm_theme_get_shadow_type (wm->theme);
 
-  shadow_cclr.red   = 0xff * shadow_clr[0] / 0xffff;
-  shadow_cclr.green = 0xff * shadow_clr[1] / 0xffff;
-  shadow_cclr.blue  = 0xff * shadow_clr[2] / 0xffff;
-  shadow_cclr.alpha = 0xff * shadow_clr[3] / 0xffff;
+  if (shadow_type == MBWM_COMP_MGR_SHADOW_NONE)
+    {
+      clutter_container_add (CLUTTER_CONTAINER (actor), texture, NULL);
+    }
+  else
+    {
+      if (shadow_type == MBWM_COMP_MGR_SHADOW_SIMPLE)
+	{
+	  mb_wm_theme_get_shadow_color (wm->theme,
+					&shadow_clr[0],
+					&shadow_clr[1],
+					&shadow_clr[2],
+					&shadow_clr[3]);
 
-  rect = clutter_rectangle_new_with_color (&shadow_cclr);
-  clutter_actor_set_position (rect, 4, 4);
-  clutter_actor_set_size (rect, geom.width, geom.height);
+	  shadow_cclr.red   = 0xff * shadow_clr[0] / 0xffff;
+	  shadow_cclr.green = 0xff * shadow_clr[1] / 0xffff;
+	  shadow_cclr.blue  = 0xff * shadow_clr[2] / 0xffff;
+	  shadow_cclr.alpha = 0xff * shadow_clr[3] / 0xffff;
 
-  clutter_actor_show (rect);
+	  rect = clutter_rectangle_new_with_color (&shadow_cclr);
+	  clutter_actor_set_position (rect, 4, 4);
+	}
+      else
+	{
+	  ClutterActor  * txt = cmgr->priv->shadow;
+	  if (!txt)
+	    {
+	      unsigned char * data;
 
-  clutter_container_add (CLUTTER_CONTAINER (actor), rect, texture, NULL);
+	      data = mb_wm_comp_mgr_clutter_shadow_gaussian_make_tile ();
+
+	      txt = g_object_new (CLUTTER_TYPE_TEXTURE, "tiled", FALSE, NULL);
+
+	      clutter_texture_set_from_rgb_data (CLUTTER_TEXTURE (txt),
+						 data,
+						 TRUE,
+						 WIDTH,
+						 HEIGHT,
+						 WIDTH*4,
+						 4,
+						 0,
+						 NULL);
+	      free (data);
+
+	      cmgr->priv->shadow = txt;
+	    }
+
+	  rect = clutter_clone_texture_new (CLUTTER_TEXTURE (txt));
+
+	  clutter_actor_set_position (rect, 2*SHADOW_RADIUS, 2*SHADOW_RADIUS);
+	}
+
+      clutter_actor_set_size (rect, geom.width, geom.height);
+      clutter_actor_show (rect);
+      clutter_container_add (CLUTTER_CONTAINER (actor), rect, texture, NULL);
+    }
+
 
   cclient->actor = actor;
   cclient->texture = texture;
@@ -1548,3 +1610,299 @@ mb_wm_comp_mgr_clutter_effect_new (MBWMCompMgrEffectType    type,
 
   return MB_WM_COMP_MGR_EFFECT (e);
 }
+
+/* ------------------------------- */
+/* Shadow Generation */
+
+typedef struct MBGaussianMap
+{
+  int	   size;
+  double * data;
+} MBGaussianMap;
+
+static double
+gaussian (double r, double x, double y)
+{
+  return ((1 / (sqrt (2 * M_PI * r))) *
+	  exp ((- (x * x + y * y)) / (2 * r * r)));
+}
+
+
+static MBGaussianMap *
+mb_wm_comp_mgr_clutter_make_gaussian_map (double r)
+{
+  MBGaussianMap  *c;
+  int	          size = ((int) ceil ((r * 3)) + 1) & ~1;
+  int	          center = size / 2;
+  int	          x, y;
+  double          t = 0.0;
+  double          g;
+
+  c = malloc (sizeof (MBGaussianMap) + size * size * sizeof (double));
+  c->size = size;
+
+  c->data = (double *) (c + 1);
+
+  for (y = 0; y < size; y++)
+    for (x = 0; x < size; x++)
+      {
+	g = gaussian (r, (double) (x - center), (double) (y - center));
+	t += g;
+	c->data[y * size + x] = g;
+      }
+
+  for (y = 0; y < size; y++)
+    for (x = 0; x < size; x++)
+      c->data[y*size + x] /= t;
+
+  return c;
+}
+
+static unsigned char
+mb_wm_comp_mgr_clutter_sum_gaussian (MBGaussianMap * map, double opacity,
+				     int x, int y, int width, int height)
+{
+  int	           fx, fy;
+  double         * g_data;
+  double         * g_line = map->data;
+  int	           g_size = map->size;
+  int	           center = g_size / 2;
+  int	           fx_start, fx_end;
+  int	           fy_start, fy_end;
+  double           v;
+  unsigned int     r;
+
+  /*
+   * Compute set of filter values which are "in range",
+   * that's the set with:
+   *	0 <= x + (fx-center) && x + (fx-center) < width &&
+   *  0 <= y + (fy-center) && y + (fy-center) < height
+   *
+   *  0 <= x + (fx - center)	x + fx - center < width
+   *  center - x <= fx	fx < width + center - x
+   */
+
+  fx_start = center - x;
+  if (fx_start < 0)
+    fx_start = 0;
+  fx_end = width + center - x;
+  if (fx_end > g_size)
+    fx_end = g_size;
+
+  fy_start = center - y;
+  if (fy_start < 0)
+    fy_start = 0;
+  fy_end = height + center - y;
+  if (fy_end > g_size)
+    fy_end = g_size;
+
+  g_line = g_line + fy_start * g_size + fx_start;
+
+  v = 0;
+  for (fy = fy_start; fy < fy_end; fy++)
+    {
+      g_data = g_line;
+      g_line += g_size;
+
+      for (fx = fx_start; fx < fx_end; fx++)
+	v += *g_data++;
+    }
+  if (v > 1)
+    v = 1;
+
+  v *= (opacity * 255.0);
+
+  r = (unsigned int) v;
+
+  return (unsigned char) r;
+}
+
+static unsigned char *
+mb_wm_comp_mgr_clutter_shadow_gaussian_make_tile ()
+{
+  unsigned char              * data;
+  int		               size;
+  int		               center;
+  int		               x, y;
+  unsigned char                d;
+  int                          pwidth, pheight;
+  double                       opacity = SHADOW_OPACITY;
+  static MBGaussianMap       * gaussian_map = NULL;
+
+  struct _mypixel
+  {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+  } * _d;
+
+
+  if (!gaussian_map)
+    gaussian_map =
+      mb_wm_comp_mgr_clutter_make_gaussian_map (SHADOW_RADIUS);
+
+  size   = gaussian_map->size;
+  center = size / 2;
+
+  /* Top & bottom */
+
+  pwidth  = MAX_TILE_SZ;
+  pheight = MAX_TILE_SZ;
+
+  data = mb_wm_util_malloc0 (4 * WIDTH * HEIGHT);
+
+  _d = (struct _mypixel*) data;
+
+  /* N */
+  for (y = 0; y < pheight; y++)
+    {
+      d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+					       center, y - center,
+					       WIDTH, HEIGHT);
+      for (x = 0; x < pwidth; x++)
+	{
+	  _d[y*3*pwidth + x + pwidth].r = 0;
+	  _d[y*3*pwidth + x + pwidth].g = 0;
+	  _d[y*3*pwidth + x + pwidth].b = 0;
+	  _d[y*3*pwidth + x + pwidth].a = d;
+	}
+
+    }
+
+  /* S */
+  pwidth = MAX_TILE_SZ;
+  pheight = MAX_TILE_SZ;
+
+  for (y = 0; y < pheight; y++)
+    {
+      d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+					       center, y - center,
+					       WIDTH, HEIGHT);
+      for (x = 0; x < pwidth; x++)
+	{
+	  _d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x + pwidth].r = 0;
+	  _d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x + pwidth].g = 0;
+	  _d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x + pwidth].b = 0;
+	  _d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x + pwidth].a = d;
+	}
+
+    }
+
+
+  /* w */
+  pwidth = MAX_TILE_SZ;
+  pheight = MAX_TILE_SZ;
+
+  for (x = 0; x < pwidth; x++)
+    {
+      d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+					       x - center, center,
+					       WIDTH, HEIGHT);
+      for (y = 0; y < pheight; y++)
+	{
+	  _d[y*3*pwidth + 3*pwidth*pheight + x].r = 0;
+	  _d[y*3*pwidth + 3*pwidth*pheight + x].g = 0;
+	  _d[y*3*pwidth + 3*pwidth*pheight + x].b = 0;
+	  _d[y*3*pwidth + 3*pwidth*pheight + x].a = d;
+	}
+
+    }
+
+  /* E */
+  for (x = 0; x < pwidth; x++)
+    {
+      d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+					       x - center, center,
+					       WIDTH, HEIGHT);
+      for (y = 0; y < pheight; y++)
+	{
+	  _d[y*3*pwidth + 3*pwidth*pheight + (pwidth-x-1) + 2*pwidth].r = 0;
+	  _d[y*3*pwidth + 3*pwidth*pheight + (pwidth-x-1) + 2*pwidth].g = 0;
+	  _d[y*3*pwidth + 3*pwidth*pheight + (pwidth-x-1) + 2*pwidth].b = 0;
+	  _d[y*3*pwidth + 3*pwidth*pheight + (pwidth-x-1) + 2*pwidth].a = d;
+	}
+
+    }
+
+  /* NW */
+  pwidth = MAX_TILE_SZ;
+  pheight = MAX_TILE_SZ;
+
+  for (x = 0; x < pwidth; x++)
+    for (y = 0; y < pheight; y++)
+      {
+	d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+						 x-center, y-center,
+						 WIDTH, HEIGHT);
+
+	_d[y*3*pwidth + x].r = 0;
+	_d[y*3*pwidth + x].g = 0;
+	_d[y*3*pwidth + x].b = 0;
+	_d[y*3*pwidth + x].a = d;
+      }
+
+  /* SW */
+  for (x = 0; x < pwidth; x++)
+    for (y = 0; y < pheight; y++)
+      {
+	d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+						 x-center, y-center,
+						 WIDTH, HEIGHT);
+
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x].r = 0;
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x].g = 0;
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x].b = 0;
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + x].a = d;
+      }
+
+  /* SE */
+  for (x = 0; x < pwidth; x++)
+    for (y = 0; y < pheight; y++)
+      {
+	d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+						 x-center, y-center,
+						 WIDTH, HEIGHT);
+
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + (pwidth-x-1) +
+	   2*pwidth].r = 0;
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + (pwidth-x-1) +
+	   2*pwidth].g = 0;
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + (pwidth-x-1) +
+	   2*pwidth].b = 0;
+	_d[(pheight-y-1)*3*pwidth + 6*pwidth*pheight + (pwidth-x-1) +
+	   2*pwidth].a = d;
+      }
+
+  /* NE */
+  for (x = 0; x < pwidth; x++)
+    for (y = 0; y < pheight; y++)
+      {
+	d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+					 x-center, y-center, WIDTH, HEIGHT);
+
+	_d[y*3*pwidth + (pwidth - x - 1) + 2*pwidth].r = 0;
+	_d[y*3*pwidth + (pwidth - x - 1) + 2*pwidth].g = 0;
+	_d[y*3*pwidth + (pwidth - x - 1) + 2*pwidth].b = 0;
+	_d[y*3*pwidth + (pwidth - x - 1) + 2*pwidth].a = d;
+      }
+
+  /* center */
+  pwidth = MAX_TILE_SZ;
+  pheight = MAX_TILE_SZ;
+
+  d = mb_wm_comp_mgr_clutter_sum_gaussian (gaussian_map, opacity,
+					   center, center, WIDTH, HEIGHT);
+
+  for (x = 0; x < pwidth; x++)
+    for (y = 0; y < pheight; y++)
+      {
+	_d[y*3*pwidth + 3*pwidth*pheight + x + pwidth].r = 0;
+	_d[y*3*pwidth + 3*pwidth*pheight + x + pwidth].g = 0;
+	_d[y*3*pwidth + 3*pwidth*pheight + x + pwidth].b = 0;
+	_d[y*3*pwidth + 3*pwidth*pheight + x + pwidth].a = d;
+      }
+
+  return data;
+}
+
